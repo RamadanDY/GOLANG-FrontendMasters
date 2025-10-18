7f40b957c6af207bace5b642558f10087db790de
RamDev <daudalaramadan@gmail.com>
2025-10-18 11:08:30 +0000

    refactor: organize code structure with added comments for clarity

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
+// global scope
 var url = "https://hellojkhgkuyh.com"
 
 func main() {
+	// functional scope
 	var message string = "hello"
 	var price float32 = 34.5
 	println(message, price, url)
 	println("helo")
+
+	{
+		// block scope
+	}
 }
